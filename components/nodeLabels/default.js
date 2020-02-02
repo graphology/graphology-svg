9fc913feda35904cd85a659eafb0c041531c03af
Alexis Jacomy <alexis.jacomy@gmail.com>
2020-02-02 17:30:41 +0100

    Linting

--- components/nodeLabels/default.js
+++ components/nodeLabels/default.js
@@ -7,7 +7,7 @@
 module.exports = function drawLabel(settings, data) {
   return (
     '<text x="' + (data.x + data.size * 1.1) + '" ' +
-          'y="' + (data.y + data.size * .25) + '" ' +
+          'y="' + (data.y + data.size / 4) + '" ' +
           'font-family="' + (settings.font || 'sans-serif') + '" ' +
           'font-size="' + data.size + '">' +
       data.label +
